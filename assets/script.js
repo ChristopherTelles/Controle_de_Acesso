776008bab945e2f9fa0e8826196f7abd71c97447
Christopher <teleschristopher@gmail.com>
2026-01-22 14:09:43 -0300

    add o botão enter como verificação

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,54 @@
-
-let choice_number = document.getElementById ("number");
+let choice_number = document.getElementById("number");
 let btn = document.getElementById("btn");
 let resposta = document.getElementById("rpt");
 
-
-btn.addEventListener("click", ()=>{
+    // função de verificação do btn "ENTER"
+function verificarAcesso() {
     let numero = Number(choice_number.value);
 
-    // LIMPA estado anterior
+    // limpa estado anterior
     resposta.classList.remove("acesso-ok", "acesso-negado");
 
-    switch (numero){
+    switch (numero) {
         case 1234:
             resposta.textContent = "Acesso Básico";
             resposta.classList.add("acesso-ok");
             break;
 
-            case 5678:
-                resposta.textContent = "Acesso Intermediário";
-                resposta.classList.add("acesso-ok");
-                break;
+        case 5678:
+            resposta.textContent = "Acesso Intermediário";
+            resposta.classList.add("acesso-ok");
+            break;
 
-                case 91011:
-                    resposta.textContent = "Acesso Administrador";
-                    resposta.classList.add("acesso-ok");
-                    break;
+        case 91011:
+            resposta.textContent = "Acesso Administrador";
+            resposta.classList.add("acesso-ok");
+            break;
 
-                    default:
-                        resposta.textContent = "Acesso Negado"
-                        resposta.classList.add("acesso-negado");
-                        break;
+        default:
+            resposta.textContent = "Acesso Negado";
+            resposta.classList.add("acesso-negado");
+            break;
     }
+}
 
-})
+// Clique no botão
+btn.addEventListener("click", verificarAcesso);
 
-// Verificação de Numerico     
-    choice_number.addEventListener("beforeinput", (e) => {
+// Enter no input
+choice_number.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        verificarAcesso();
+    }
+});
+
+// Verificação de Numérico
+choice_number.addEventListener("beforeinput", (e) => {
     if (e.data && !/^\d+$/.test(e.data)) {
         e.preventDefault();
     }
 });
 
-
-    choice_number.addEventListener("input", () => {
-        choice_number.value = choice_number.value.replace(/\D/g, "");
-    })
+choice_number.addEventListener("input", () => {
+    choice_number.value = choice_number.value.replace(/\D/g, "");
+});
